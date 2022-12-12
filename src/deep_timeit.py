7783efec378d8306fc5a38d2587fedb52ec809e3
Oliver Gibson <ojrgibson@perse.co.uk>
2022-12-12 20:40:54 +0000

    Bug fixes

--- src/deep_timeit.py
+++ src/deep_timeit.py
@@ -5,6 +5,8 @@ import tkinter
 import colorsys
 import matplotlib
 
+CHUNK_ADJACENCIES = {"if ": ["elif ", "else:"], "try:": ["except ", "except:", "finally:"]}
+
 class Time():
     def __init__(self, start, end, time, indentation, nextindentation=None):
         self.start = start
@@ -23,11 +25,23 @@ class Info():
         self.removed = removed
     
     def show(self, mintimetotrigger=None):
+        MAXX = 100
+        MAXY = 30
         root = tkinter.Tk()
         root.title(" Function display ")
-        Output = tkinter.Text(root)
-        for line in self.lines:
-            Output.insert(tkinter.INSERT, line+"\n")
+
+        scroll_v = tkinter.Scrollbar(root)
+        scroll_v.pack(side = tkinter.RIGHT, fill = "y")
+        scroll_h = tkinter.Scrollbar(root, orient = tkinter.HORIZONTAL)
+        scroll_h.pack(side = tkinter.BOTTOM, fill = "x")
+
+        Output = tkinter.Text(root, width = min(MAXX, max([len(i) for i in self.lines])), height = min(MAXY, len(self.lines)), yscrollcommand = scroll_v.set, xscrollcommand = scroll_h.set, wrap = tkinter.NONE)
+
+        scroll_h.config(command = Output.xview)
+        scroll_v.config(command = Output.yview)
+
+        for index, line in enumerate(self.lines):
+            Output.insert(tkinter.INSERT, line+("\n" if index != len(self.lines)-1 else ""))
         mintime = 0
         maxtime = max(self.times, key=lambda x: x.time).time
         for index, timeset in enumerate(self.times):
@@ -57,6 +71,16 @@ def deepTimeit(func, args=[], kwargs={}, reattempt=True, show=True, mintime=None
     linetimevar = "linetime"
     maxrepeats = 100000
     lines = inspector.getsource(func).rstrip().split("\n")
+    newlines = []
+    for line in lines:
+        validline = True
+        if line.lstrip() == "":
+            validline = False
+        if line.lstrip().startswith("#"):
+            validline = False
+        if validline:
+            newlines.append(line)
+    lines = newlines
     caller_frame = inspector.stack()[1]
     caller_module = inspector.getmodule(caller_frame[0])
     start = lines[0]
@@ -110,13 +134,14 @@ def deepTimeit(func, args=[], kwargs={}, reattempt=True, show=True, mintime=None
                 newlines.append(ind+f"    {alltimesvar}[{end}] += time.time()-{linetimevar}{end}")
                 newlines.append(ind+f"    {allcountsvar}[{end}] += 1")
         
-        if newlines[-1].lstrip().startswith("return"):
-            if newlines[-1].lstrip().startswith("return "):
-                newlines[-1] += f", {alltimesvar}, {allcountsvar}"
+        for newlineindex, newline in enumerate(newlines):
+            if newline.lstrip().startswith("return"):
+                if newline.lstrip().startswith("return "):
+                    newlines[newlineindex] += f", {alltimesvar}, {allcountsvar}"
+                else:
+                    newlines[newlineindex] += f" {alltimesvar}, {allcountsvar}"
             else:
-                newlines[-1] += f" {alltimesvar}, {allcountsvar}"
-        else:
-            newlines.append(f"{getIndentation(lines[1])}return {alltimesvar}, {allcountsvar}")
+                newlines.append(f"{getIndentation(lines[1])}return {alltimesvar}, {allcountsvar}")
 
         
         lines = "\n".join(newlines)
@@ -124,14 +149,25 @@ def deepTimeit(func, args=[], kwargs={}, reattempt=True, show=True, mintime=None
         #print(strtoexec)
         localcopy = locals()
         globalcopy = globals()
-        globalcopy.update(caller_module.__dict__)
-        exec(strtoexec, globalcopy, localcopy)
+        try:
+            globalcopy.update(caller_module.__dict__)
+        except AttributeError:
+            pass
+        try:
+            exec(strtoexec, globalcopy, localcopy)
+        except SyntaxError:
+            print(strtoexec)
+            raise SyntaxError
         funcname = func.__name__
         exec(f"totaltime = time.time()\nreturnval = {funcname}(*args, **kwargs)\ntotaltime = time.time()-totaltime", globals(), localcopy)
         results = localcopy["returnval"]
         totaltime = localcopy["totaltime"]
-        counts = results[-1]
-        times = results[-2]
+        try:
+            counts = results[-1]
+            times = results[-2]
+        except TypeError:
+            print(strtoexec)
+            raise TypeError
         maxx = max(counts.values())
         if maxx == maxrepeats and reattempt:
             needsToRedo = True
@@ -168,11 +204,34 @@ def getChunksToTime(lines):
         if nextlineindentation <= lineindentation:
             indices.append([index, index])
         else:
+            adjacentchunktitles = []
+            for i in CHUNK_ADJACENCIES:
+                for j in CHUNK_ADJACENCIES[i]:
+                    adjacentchunktitles.append(j)
+            shouldcont = False
+            for i in adjacentchunktitles:
+                if lines[index].lstrip().startswith(i):
+                    shouldcont = True
+            if shouldcont:
+                continue
             nextIndexWhereLEQ = len(lines)-1
             for newindex in range(index+1, len(lines)):
                 if getIndentation(lines[newindex]) <= lineindentation:
-                    nextIndexWhereLEQ = newindex-1
-                    break
+                    partofadjacent = False
+                    for key in CHUNK_ADJACENCIES:
+                        if lines[index].lstrip().startswith(key):
+                            partofadjacent = CHUNK_ADJACENCIES[key]
+                    if partofadjacent != False:
+                        oneofadjacent = False
+                        for potadj in partofadjacent:
+                            if lines[newindex][len(getIndentation(lines[index])):].startswith(potadj):
+                                oneofadjacent = True
+                        if not(oneofadjacent):
+                            nextIndexWhereLEQ = newindex-1
+                            break    
+                    else:
+                        nextIndexWhereLEQ = newindex-1
+                        break
             indices.append([index, nextIndexWhereLEQ])
     
     return indices
@@ -191,3 +250,31 @@ def getIndentation(line):
         return " "*line.index(line.lstrip()[0])
     except:
         return ""
+
+def factorial(a, b, extraadd = True):
+    import random
+    t = 1
+    time.sleep(0.05)
+    time.sleep(1/20)
+    try:
+        x = 1
+    except:
+        y = 1
+    #asdf
+    for i in range(1, a*b):
+
+        t *= i
+        x = 0
+        y = 0
+        if extraadd:
+            for i in range(100000):
+
+                y += i
+
+                if i < 500:
+                    x += i
+                    x += random.randint(1, 100)
+    return t
+
+deepTimeit(deepTimeit, args=[factorial], kwargs={"args":[5, 5]})
+#deepTimeit(factorial, args=[5, 5])
