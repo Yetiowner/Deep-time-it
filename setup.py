f49f0dbd0886f96f59dfccf9cb64c948edafc7ec
Oliver Gibson <ojrgibson@perse.co.uk>
2022-12-27 16:41:53 +0000

    Updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ BUILD_ID = os.environ.get("BUILD_BUILDID", "0")
 
 setup(
     name="deep_timeit",
-    version="0.1" + "." + BUILD_ID,
+    version="1.0" + "." + BUILD_ID,
     # Author details
     author="Oliver Gibson",
     author_email="ojrgibson@perse.co.uk",
